661f4a760863d26f254a8ec2e894938571aac32d
Carlos <carlos.dominguezalcazar@alum.uca.es>
2022-11-28 15:02:42 +0100

    añadido boton de collapse con estilo

--- src/componentes/Escena.jsx
+++ src/componentes/Escena.jsx
@@ -3,6 +3,8 @@ import AnadirEscena from "./AnadirEscena";
 import Respuesta from "./Respuesta";
 import { NodoEscenarioContext } from "../context/NodoEscenarioContext";
 import AnadirRespuesta from "./AnadirRespuesta";
+import { BsChevronRight } from "react-icons/bs";
+import { Collapse } from "react-bootstrap";
 
 export default function Escena(props) {
   const {
@@ -12,6 +14,7 @@ export default function Escena(props) {
     modificable = true,
   } = props;
   const [escenaDatos, setEscenaDatos] = useState(null);
+  const [colapsado, setColapsado] = useState(false);
   const { nodo, setNodo } = useContext(NodoEscenarioContext);
 
   useEffect(() => {
@@ -43,87 +46,117 @@ export default function Escena(props) {
 
   return escenaDatos ? (
     <>
-      <div
-        id="escena"
-        onClick={() => handleClick()}
-        style={
-          !modificable && !escenaDatos.respuestas.length
-            ? nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
-              ? { backgroundColor: "#60e78f6e", marginBottom: 10 }
-              : { marginBottom: 10 }
-            : nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
-            ? { backgroundColor: "#60e78f6e" }
-            : {}
-        }
-        className={
-          escenaDatos.escena_tipo_id === 4
-            ? !modificable && !escenaDatos.respuestas.length
-              ? "escena-1-2-3"
-              : "escena-4"
-            : "escena-1-2-3"
-        }
-      >
-        <b>{escenaDatos.titulo}</b>
-      </div>
-      <div id="escena-respuestas" className="d-flex">
+      <div className="d-flex">
         <div
-          id="escena-respuestas-espacio"
-          className={
-            escenaDatos.escena_tipo_id === 4
-              ? !modificable && !escenaDatos.respuestas.length
-                ? ""
-                : "escena-respuestas-espacio-borde"
-              : ""
-          }
+          id="escena"
+          onClick={() => handleClick()}
           style={
-            nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
-              ? { borderColor: "#60e78f6e" }
+            !modificable && !escenaDatos.respuestas.length
+              ? nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
+                ? { backgroundColor: "#60e78f6e", marginBottom: 10 }
+                : { marginBottom: 10 }
+              : nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
+              ? { backgroundColor: "#60e78f6e" }
               : {}
           }
-        ></div>
+          className={
+            escenaDatos.escena_tipo_id === 4 && !colapsado
+              ? !modificable && !escenaDatos.respuestas.length
+                ? "escena-1-2-3"
+                : "escena-4"
+              : "escena-1-2-3"
+          }
+        >
+          <b>{escenaDatos.titulo}</b>
+        </div>
 
         <div
-          className={
-            escenaDatos.escena_tipo_id === 4
-              ? "w-100 escena-respuestas-4"
-              : "w-100"
-          }
+          id="escena-collapse-btn"
+          className="collapse-btn"
+          onClick={() => setColapsado(!colapsado)}
           style={
-            !modificable && escenaDatos.escena_tipo_id === 4
-              ? { paddingBottom: 5 }
+            !modificable && !escenaDatos.respuestas.length
+              ? nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
+                ? { backgroundColor: "#60e78f6e", marginBottom: 10 }
+                : { marginBottom: 10 }
+              : nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
+              ? { backgroundColor: "#60e78f6e" }
               : {}
           }
         >
-          {escenaDatos.respuestas.length !== 0 ? (
-            <>
-              {escenaDatos.respuestas.map((respuesta) => {
-                return (
-                  <Respuesta
-                    key={respuesta.id}
-                    respuesta={respuesta}
-                    handleEliminarRespuesta={handleEliminarRespuesta}
-                    escenarioId={escenarioId}
-                    modificable={modificable}
-                  />
-                );
-              })}
-              {modificable && escenaDatos.escena_tipo_id === 4 && (
-                <AnadirRespuesta
-                  escenaId={escenaDatos.id}
-                  handleAnadirRespuesta={handleAnadirRespuesta}
-                />
-              )}
-            </>
-          ) : (
-            modificable && (
-              <AnadirRespuesta
-                escenaId={escenaDatos.id}
-                handleAnadirRespuesta={handleAnadirRespuesta}
-              />
-            )
-          )}
+          <BsChevronRight
+            style={
+              colapsado
+                ? { transform: "rotate(0deg)", transition: "0.35s" }
+                : { transform: "rotate(90deg)", transition: "0.35s" }
+            }
+          />
         </div>
       </div>
+
+      <Collapse in={!colapsado}>
+        <div>
+          <div id="escena-respuestas" className="d-flex">
+            <div
+              id="escena-respuestas-espacio"
+              className={
+                escenaDatos.escena_tipo_id === 4
+                  ? !modificable && !escenaDatos.respuestas.length
+                    ? ""
+                    : "escena-respuestas-espacio-borde"
+                  : ""
+              }
+              style={
+                nodo && nodo.tipo === "escena" && nodo.id === escenaDatos.id
+                  ? { borderColor: "#60e78f6e" }
+                  : {}
+              }
+            ></div>
+
+            <div
+              className={
+                escenaDatos.escena_tipo_id === 4
+                  ? "w-100 escena-respuestas-4"
+                  : "w-100"
+              }
+              style={
+                !modificable && escenaDatos.escena_tipo_id === 4
+                  ? { paddingBottom: 5 }
+                  : {}
+              }
+            >
+              {escenaDatos.respuestas.length !== 0 ? (
+                <>
+                  {escenaDatos.respuestas.map((respuesta) => {
+                    return (
+                      <Respuesta
+                        key={respuesta.id}
+                        respuesta={respuesta}
+                        handleEliminarRespuesta={handleEliminarRespuesta}
+                        escenarioId={escenarioId}
+                        modificable={modificable}
+                      />
+                    );
+                  })}
+                  {modificable && escenaDatos.escena_tipo_id === 4 && (
+                    <AnadirRespuesta
+                      escenaId={escenaDatos.id}
+                      handleAnadirRespuesta={handleAnadirRespuesta}
+                    />
+                  )}
+                </>
+              ) : (
+                modificable && (
+                  <AnadirRespuesta
+                    escenaId={escenaDatos.id}
+                    handleAnadirRespuesta={handleAnadirRespuesta}
+                  />
+                )
+              )}
+            </div>
+          </div>
+        </div>
+      </Collapse>
     </>
   ) : (
     modificable && (
--- src/componentes/Respuesta.jsx
+++ src/componentes/Respuesta.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Collapse } from "react-bootstrap";
 import { NodoEscenarioContext } from "../context/NodoEscenarioContext";
+import { BsChevronRight } from "react-icons/bs";
 import Escena from "./Escena";
 
 export default function Respuesta(props) {
@@ -10,6 +12,7 @@ export default function Respuesta(props) {
     modificable = true,
   } = props;
   const [respuestaDatos, setRespuestaDatos] = useState(null);
+  const [colapsado, setColapsado] = useState(false);
   const { nodo, setNodo } = useContext(NodoEscenarioContext);
 
   useEffect(() => {
@@ -29,34 +32,60 @@ export default function Respuesta(props) {
   return (
     respuestaDatos && (
       <>
-        <div
-          id="respuesta"
-          onClick={() => handleClick()}
-          style={
-            nodo && nodo.tipo === "respuesta" && nodo.id === respuestaDatos.id
-              ? { backgroundColor: "#60e78f6e" }
-              : {}
-          }
-        >
-          <b>Respuesta </b>
-          {respuestaDatos.valores}
-        </div>
-        <div id="respuesta-escena">
-          {respuestaDatos.escena ? (
-            <Escena
-              escena={respuestaDatos.escena}
-              escenarioId={escenarioId}
-              respuestaId={respuestaDatos.id}
-              modificable={modificable}
+        <div className="d-flex">
+          <div
+            id="respuesta"
+            onClick={() => handleClick()}
+            style={
+              nodo && nodo.tipo === "respuesta" && nodo.id === respuestaDatos.id
+                ? { backgroundColor: "#60e78f6e" }
+                : {}
+            }
+          >
+            <b>Respuesta </b>
+            {respuestaDatos.valores}
+          </div>
+          <div
+            id="respuesta-collapse-btn"
+            className="collapse-btn"
+            onClick={() => setColapsado(!colapsado)}
+            style={
+              nodo && nodo.tipo === "respuesta" && nodo.id === respuestaDatos.id
+                ? { backgroundColor: "#60e78f6e" }
+                : {}
+            }
+          >
+            <BsChevronRight
+              style={
+                colapsado
+                  ? { transform: "rotate(0deg)", transition: "0.35s" }
+                  : { transform: "rotate(90deg)", transition: "0.35s" }
+              }
             />
-          ) : (
-            <Escena
-              escenarioId={escenarioId}
-              respuestaId={respuestaDatos.id}
-              modificable={modificable}
-            />
-          )}
+          </div>
+          <div></div>
         </div>
+
+        <Collapse in={!colapsado}>
+          <div>
+            <div id="respuesta-escena">
+              {respuestaDatos.escena ? (
+                <Escena
+                  escena={respuestaDatos.escena}
+                  escenarioId={escenarioId}
+                  respuestaId={respuestaDatos.id}
+                  modificable={modificable}
+                />
+              ) : (
+                <Escena
+                  escenarioId={escenarioId}
+                  respuestaId={respuestaDatos.id}
+                  modificable={modificable}
+                />
+              )}
+            </div>
+          </div>
+        </Collapse>
       </>
     )
   );
