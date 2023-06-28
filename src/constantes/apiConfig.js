3b491ed5c825536d6d128bf94b5a612c667a3641
Carlos <carlos.dominguezalcazar@alum.uca.es>
2023-06-29 00:50:01 +0200

    api config

--- src/constantes/apiConfig.js
+++ src/constantes/apiConfig.js
@@ -1,2 +1,2 @@
 export const apiURL = "http://localhost:8000/api/";
-// export const apiURL = "https://virtualedu-api-production.up.railway.app/api/";
+// export const apiURL = "http://16.171.17.23/api/";
