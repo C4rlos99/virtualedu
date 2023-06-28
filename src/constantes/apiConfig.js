76aa42a62e6ab4b125fb20fe54025cfdced56547
Carlos <carlos.dominguezalcazar@alum.uca.es>
2023-06-29 01:29:35 +0200

    api ip

--- src/constantes/apiConfig.js
+++ src/constantes/apiConfig.js
@@ -1,2 +1,2 @@
-export const apiURL = "http://localhost/api/";
-// export const apiURL = "http://16.171.17.23/api/";
+// export const apiURL = "http://localhost:8000/api/";
+export const apiURL = "http://16.171.17.23/api/";
